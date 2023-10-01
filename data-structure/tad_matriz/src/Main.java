b8477d3b14f8bddac49ec85ceb5d5c9bc31befe1
Robert <robertgleison12@gmail.com>
2023-10-01 12:38:02 +0100

    Fixed errors in multiply function

--- data-structure/tad_matriz/src/Main.java
+++ data-structure/tad_matriz/src/Main.java
@@ -1,35 +1,35 @@
 import java.util.Scanner;
 
-class TestMatrix {
-    public static void main(String[] args) {
-        Scanner stdin = new Scanner(System.in);
-
-        Matrix m1 = Matrix.identity(5);
-        System.out.println(m1);
-
-        Matrix m2 = new Matrix(stdin.nextInt(), stdin.nextInt());
-        m2.read(stdin);
-        System.out.println(m2);
-        Matrix m3 = new Matrix(stdin.nextInt(), stdin.nextInt());
-        m3.read(stdin);
-        System.out.println(m3);
-        Matrix m4 = new Matrix(stdin.nextInt(), stdin.nextInt());
-        m4.read(stdin);
-        System.out.println(m4);
-
-        Matrix m5 = m1.transpose();
-        System.out.println(m5);
-        Matrix m6 = m2.transpose();
-        System.out.println(m6);
-
-        Matrix m7 = m2.sum(m3);
-        System.out.println(m7);
-        Matrix m8 = m2.multiply(m4);
-        System.out.println(m8);
-    }
-}
-
-
+//class TestMatrix {
+//    public static void main(String[] args) {
+//        Scanner stdin = new Scanner(System.in);
+//
+//        Matrix m1 = Matrix.identity(5);
+//        System.out.println(m1);
+//
+//        Matrix m2 = new Matrix(stdin.nextInt(), stdin.nextInt());
+//        m2.read(stdin);
+//        System.out.println(m2);
+//        Matrix m3 = new Matrix(stdin.nextInt(), stdin.nextInt());
+//        m3.read(stdin);
+//        System.out.println(m3);
+//        Matrix m4 = new Matrix(stdin.nextInt(), stdin.nextInt());
+//        m4.read(stdin);
+//        System.out.println(m4);
+//
+//        Matrix m5 = m1.transpose();
+//        System.out.println(m5);
+//        Matrix m6 = m2.transpose();
+//        System.out.println(m6);
+//
+//        Matrix m7 = m2.sum(m3);
+//        System.out.println(m7);
+//        Matrix m8 = m2.multiply(m4);
+//        System.out.println(m8);
+//    }
+//}
+//
+//
 class Matrix {
     int data[][]; // os elementos da matriz em si
     int rows;     // numero de linhas
@@ -95,16 +95,22 @@ class Matrix {
     }
 
     public Matrix multiply(Matrix m) {
-        Matrix mat = new Matrix(this.rows, m.cols);
-        if (this.cols == m.rows) {
-            for (int i = 0; i < this.rows; i++) {
+        if (this.cols != m.rows) {
+            throw new IllegalArgumentException("Matrix dimensions are not compatible for multiplication.");
+        }
+
+        Matrix result = new Matrix(this.rows, m.cols);
+
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < m.cols; j++) {
                 int sum = 0;
-                for (int j = 0; j < this.cols; j++) {
-                    sum += this.data[i][j] + m.data[j][i];
-                    mat.data[i][j] = sum;
+                for (int k = 0; k < this.cols; k++) {
+                    sum += this.data[i][k] * m.data[k][j];
                 }
+                result.data[i][j] = sum;
             }
         }
-        return mat;
+
+        return result;
     }
 }
