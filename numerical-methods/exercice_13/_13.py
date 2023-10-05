2e9253e71fef204e523ec5a519a150d468b1a08f
Robert <robertgleison12@gmail.com>
2023-10-05 13:32:09 +0100

    Optimized question A

--- numerical-methods/exercice_13/_13.py
+++ numerical-methods/exercice_13/_13.py
@@ -1,40 +1,48 @@
-#Question A
+# #Question A
+
+
+def calculate_term(n_term):
+    return ((-1)**n_term)*(n_term/((5**n_term) + 10))
+
+
 def calculate_serie_a(min_error):
     sum = 0
     n_term = 1
-    term = ((-1)**n_term)*(n_term/((5**n_term) + 10))
-
-    while(abs(term) >= min_error):
-        sum += term
+    while(abs(calculate_term(n_term)) >= min_error):
+        sum += calculate_term(n_term)
         n_term+=1
-        term = ((-1)**n_term)*(n_term/((5**n_term) + 10))
-
     #Truncate the number
     sum = (sum//min_error) * min_error
     print(sum)
 calculate_serie_a(5e-4)
 print()
 
-#Question B
-#Empiric method
-def calculate_serie_b(min_error):
-    sum = 0
-    n_term = 1
-    term = 1/(n_term*(3**n_term))
-    n_term+=1
-    next_term = 1/(n_term*(3**n_term))
-    sum += term
-    while(abs(next_term-term)>= min_error):
-        term = next_term
-        n_term+=1
-        next_term = 1/(n_term*(3**n_term))
-        sum += term
-        #Truncate the number
-    sum = (sum//min_error) * min_error
-    print(sum)
+# #Question A - optimized
 
-calculate_serie_b(5e-4)
-print()
 
-#Question C
-#Empiric method
+
+# #Question B
+# #Empiric method
+# def calculate_serie_b(min_error):
+#     sum = 0
+#     n_term = 1
+#     term = 1/(n_term*(3**n_term))
+#     n_term+=1
+#     next_term = 1/(n_term*(3**n_term))
+#     sum += term
+#     while(abs(next_term-term)>= min_error):
+#         term = next_term
+#         n_term+=1
+#         next_term = 1/(n_term*(3**n_term))
+#         sum += term
+#         #Truncate the number
+#     sum = (sum//min_error) * min_error
+#     print(sum)
+
+# calculate_serie_b(5e-4)
+# print()
+
+# #Question C
+# #Empiric method
+
+
