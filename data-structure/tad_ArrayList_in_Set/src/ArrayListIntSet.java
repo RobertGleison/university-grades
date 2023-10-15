df319ac4e9b604acfbd387967610434ebd746ed4
Robert <robertgleison12@gmail.com>
2023-10-15 02:35:22 +0100

    Implemented Early Return

--- data-structure/tad_ArrayList_in_Set/src/ArrayListIntSet.java
+++ data-structure/tad_ArrayList_in_Set/src/ArrayListIntSet.java
@@ -1,45 +1,45 @@
 public class ArrayListIntSet implements IntSet {
-    private int size;   // Numero de elementos do conjunto
-    private int elem[]; // Array que contem os elementos em si
+    private int size;
+    private int elem[];
 
     ArrayListIntSet(int maxSize) {
         elem = new int[maxSize];
         size = 0;
     }
 
-    public boolean add(int x) {
-        if (!contains(x)) {
-            if (size == elem.length){
-                // throw new RuntimeException("Maximum size of set reached");
-                int new_array[] = new int[size*2];
-                for (int i = 0; i<size; i++)
-                    new_array[i] = elem[i];
-                elem = new_array;
-            }
-            elem[size] =  x;
-            size++;
-            return true;
-        }
-        return false;
-    }
-
-    public boolean remove(int x) {
-        if (contains(x)) {
-            int pos = 0;
-            while (elem[pos] != x) pos++;
-            size--;
-            elem[pos] = elem[size]; // Trocar ultimo elemento
-            return true;            // com o que se removeu
-        }
-        return false;
-    }
-
     public boolean contains(int x) {
-        for (int i=0; i<size; i++)
+        for (int i = 0; i < size; i++)
             if (elem[i] == x)
                 return true;
         return false;
     }
+
+    public boolean add(int x) {
+        if (contains(x)) return false;
+        else {
+            if (size == elem.length) {
+                int new_array[] = new int[size * 2];
+                for (int i = 0; i < size; i++)
+                    new_array[i] = elem[i];
+                elem = new_array;
+            }
+            elem[size] = x;
+            size++;
+            return true;
+        }
+    }
+
+    public boolean remove(int x) {
+        if (!contains(x)) return false;
+        else {
+            int pos = 0;
+            while (elem[pos] != x) pos++;
+            size--;
+            elem[pos] = elem[size];
+            return true;
+        }
+    }
+
 
     public void clear() {
         size = 0;
@@ -52,30 +52,29 @@ public class ArrayListIntSet implements IntSet {
     @Override
     public String toString() {
         String res = "{";
-        for (int i=0; i<size; i++) {
-            if (i>0) res += ",";
+        for (int i = 0; i < size; i++) {
+            if (i > 0) res += ",";
             res += elem[i];
         }
         res += "}";
         return res;
     }
 
-    /* Method Implemented - Not Pre-Given */
     public boolean equals(IntSet s) {
         if (size() != s.size()) return false;
         else {
-            for (int i = 0; i<size; i++)
+            for (int i = 0; i < size; i++)
                 if (!s.contains(elem[i]))
                     return false;
         }
         return true;
     }
 
-    /* Method Implemented - Not Pre-Given */
     public IntSet intersection(IntSet s) {
-        IntSet new_set =  new ArrayListIntSet(1);
-        for (int i = 0; i<size(); i++)
+        IntSet new_set = new ArrayListIntSet(1);
+        for (int i = 0; i < size(); i++)
             if (s.contains(elem[i]) && contains(elem[i]))
                 new_set.add(elem[i]);
         return new_set;
-    }}
+    }
+}
--- data-structure/tad_ArrayList_in_Set/src/Main.java
+++ data-structure/tad_ArrayList_in_Set/src/Main.java
@@ -1,25 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        IntSet s1 = new ArrayListInSet(100);
-        IntSet s2 = new ArrayListInSet(100);
+        //Test ArrayListIntSet
+//        IntSet s1 = new ArrayListIntSet(100);
+//        IntSet s2 = new ArrayListIntSet(100);
+//
+//        for (int i=1; i<=5; i++)
+//            s1.add(i);
+//        System.out.println("s1 = " + s1 + " | tamanho = " + s1.size());
+//
+//        for (int i=3; i<=7; i++)
+//            s2.add(i);
+//        System.out.println("s2 = " + s2 + " | tamanho = " + s2.size());
+//
+//        IntSet s3 = s1.intersection(s2);
+//        System.out.println("s3 = " + s3 + " | tamanho = " + s3.size());
+//
+//        IntSet s4 = s2.intersection(s1);
+//        System.out.println("s4 = " + s4 + " | tamanho = " + s4.size());
+//
+//        System.out.println("s1.equals(s2) = " + s1.equals(s2));
+//        System.out.println("s3.equals(s4) = " + s3.equals(s4));
 
-        for (int i=1; i<=5; i++)
-            s1.add(i);
-        System.out.println("s1 = " + s1 + " | tamanho = " + s1.size());
+        //Test BooleanArrayIntSet
+        int n = 1000000;
+        IntSet s = new BooleanArrayintSet(n);
+        boolean tmp;
 
-        for (int i=3; i<=7; i++)
-            s2.add(i);
-        System.out.println("s2 = " + s2 + " | tamanho = " + s2.size());
+        System.out.println("Adicionando todos os numeros entre 1 e " + n + "...");
+        for (int i=1; i<=n; i++)
+            tmp = s.add(i);
 
-        IntSet s3 = s1.intersection(s2);
-        System.out.println("s3 = " + s3 + " | tamanho = " + s3.size());
+        System.out.println("Adicionando novamente todos os numeros entre 1 e " + n + "...");
+        for (int i=1; i<=n; i++)
+            tmp = s.add(i);
 
-        IntSet s4 = s2.intersection(s1);
-        System.out.println("s4 = " + s4 + " | tamanho = " + s4.size());
+        System.out.println("Verificando se todos os numeros entre 1 e " + n + " existem...");
+        for (int i=1; i<=n; i++)
+            tmp = s.contains(i);
 
-        System.out.println("s1.equals(s2) = " + s1.equals(s2));
-        System.out.println("s3.equals(s4) = " + s3.equals(s4));
+        System.out.println("Removendo todos os numeros entre 1 e " + n + "...");
+        for (int i=1; i<=n; i++)
+            tmp = s.remove(i);
     }
+
     }
+
 
 
